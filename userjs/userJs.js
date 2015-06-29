992dfc69b2163ed8eb7d47203464789efa9307bf
joemar27 <josephmar.hernandez@gmail.com>
2015-06-29 10:09:08 -0500

    Initial commit

---
+++ userjs/userJs.js
@@ -0,0 +1,14 @@
+function profilePage(){
+    document.getElementById('kcImage').style.display = "none";
+    document.getElementById('aboutMe').style.display = "none";
+    document.getElementById('contactInfo').style.display = "none";
+    document.getElementById('myGallery').style.display = "block";
+}
+
+function contactPage(){
+    document.getElementById('kcImage').style.display = "none";
+    document.getElementById('aboutMe').style.display = "none";
+    document.getElementById('contactInfo').style.display = "none";
+    document.getElementById('myGallery').style.display = "none";
+    document.getElementById('contactInfo').style.display = "block";   
+}
